478d5678bb928a4e98e09260e12a4a2c88a5c745
student <student@PC111S27.fgakusei.local>
2014-06-19 14:15:24 +0900

    06/19

--- gen/jp/ac/st/asojuku/original2014002/R.java
+++ gen/jp/ac/st/asojuku/original2014002/R.java
@@ -24,10 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int LvHITOKOTO=0x7f080008;
+        public static final int action_settings=0x7f08000b;
         public static final int btnCHECK=0x7f080006;
+        public static final int btnDELETE=0x7f080009;
         public static final int btnENTRY=0x7f080005;
         public static final int btnMAINTE=0x7f080004;
+        public static final int btnMAINTE_BACK=0x7f08000a;
         public static final int edtMsg=0x7f080003;
         public static final int imageView1=0x7f080007;
         public static final int linearLayout1=0x7f080000;
